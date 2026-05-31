5320eb0a18f1515f87dac2125c8e7c63943ee784
Natriy228 <131760377+Natriy228@users.noreply.github.com>
2026-05-31 14:24:25 +0300

    some changes (idk)

--- pages/main/index.js
+++ pages/main/index.js
@@ -19,7 +19,7 @@ export class MainPage {
     getHTML() {
         return (
             `
-                <div id="main-page" class="d-flex flex-wrap" style="justify-content: left"><div/>
+                <div class="d-flex flex-wrap" style="justify-content: center"><div id="main-page" class="d-flex flex-wrap" style="justify-content: left; width: 85%"></div></div>
             `
         );
     }
