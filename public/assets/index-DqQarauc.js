f06df8d0774028d631fc7258712a9c6d5519b8a2
Natriy228 <131760377+Natriy228@users.noreply.github.com>
2026-05-31 14:40:26 +0300

    static page replaced

---
+++ public/assets/index-DqQarauc.js
@@ -0,0 +1,104 @@
+var e=(e,t)=>()=>(e&&(t=e(e=0)),t),t=(e,t)=>()=>(t||(e((t={exports:{}}).exports,t),e=null),t.exports);(function(){let e=document.createElement(`link`).relList;if(e&&e.supports&&e.supports(`modulepreload`))return;for(let e of document.querySelectorAll(`link[rel="modulepreload"]`))n(e);new MutationObserver(e=>{for(let t of e)if(t.type===`childList`)for(let e of t.addedNodes)e.tagName===`LINK`&&e.rel===`modulepreload`&&n(e)}).observe(document,{childList:!0,subtree:!0});function t(e){let t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin===`use-credentials`?t.credentials=`include`:e.crossOrigin===`anonymous`?t.credentials=`omit`:t.credentials=`same-origin`,t}function n(e){if(e.ep)return;e.ep=!0;let n=t(e);fetch(e.href,n)}})();var n,r=e((()=>{n=class{constructor(e){this.parent=e}getCardHTML(e){return`
+            <div class="card">
+                <img class="card-img-top" src="${e.src}" alt="${e.im_desc}">
+                <div class="card-body">
+                    <h5 class="card-title">${e.title}</h5>
+                    <p class="card-text">${e.text}</p>
+                    <table>
+                        <tr>
+                            <td>
+                                <button class="btn btn-primary" id="click-card-${e.id}" data-id="${e.id}">Припарковать</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-primary" id="delete-card-${e.id}" data-id="${e.id}">Удалить</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <button class="btn btn-primary" id="edit-card-${e.id}" data-id="${e.id}">Редактировать</button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        `}addClickListener(e,t){document.getElementById(`click-card-${e.id}`).addEventListener(`click`,t)}addDeleteListener(e,t){document.getElementById(`delete-card-${e.id}`).addEventListener(`click`,t)}addEditListener(e,t){document.getElementById(`edit-card-${e.id}`).addEventListener(`click`,t)}render(e,t,n,r){let i=this.getCardHTML(e);this.parent.insertAdjacentHTML(`beforeend`,i),this.addClickListener(e,t),this.addDeleteListener(e,n),this.addEditListener(e,r)}}})),i,a=e((()=>{i=class{constructor(e){this.parent=e}getHTML(e){return`
+                <div class="card" style="width: 100%;">
+                    <img src="${e.page_src}" style="height:100px; object-fit: none" class="img-fluid" alt="${e.im_desc}">
+                    <div class="card-body">
+                        <h5 class="card-title">${e.title}</h5>
+                        <p class="card-text">${e.text}</p>
+                    </div>
+                </div>
+            `}render(e){let t=this.getHTML(e);this.parent.insertAdjacentHTML(`beforeend`,t)}}})),o,s=e((()=>{C(),o=class{constructor(e){this.parent=e}getHTML(){return`
+        <div class="header" style="padding-left: 7px; padding-top: 7px; margin-bottom: 10px">
+            <button class="btn btn-primary" id="home_button"><image src="../../resources/images/homev.png" width="40" height="40"></button>
+        </div>
+        `}returnToMainPage(){new S(document.getElementById(`root`)).render(!1)}addHomeListener(e){document.getElementById(`home_button`).addEventListener(`click`,e)}render(){let e=this.getHTML();this.parent.insertAdjacentHTML(`afterBegin`,e),this.addHomeListener(this.returnToMainPage)}}})),c,l,u=e((()=>{c=class{get=async(e,t)=>{try{let n=await fetch(e);t(await n.json(),n.statusText)}catch(e){console.log(e)}};post=async(e,t,n)=>{try{let r=await fetch(e,{method:`POST`,headers:{"Content-Type":`application/json;charset=utf-8`},body:JSON.stringify(t)});n(await r.text(),r.statusText)}catch(e){console.log(e)}};patch=async(e,t,n)=>{try{let r=await fetch(e,{method:`PATCH`,headers:{"Content-Type":`application/json;charset=utf-8`},body:JSON.stringify(t)});n(await r.text(),r.statusText)}catch(e){console.log(e)}};delete=async(e,t)=>{try{let n=await fetch(e,{method:`DELETE`});t(await n.text(),n.statusText)}catch(e){console.log(e)}};_handleResponse(e,t){try{t(e.responseText?JSON.parse(e.responseText):null,e.status)}catch(n){console.error(`Ошибка парсинга JSON:`,n),t(null,e.status)}}},l=new c})),d,f,p=e((()=>{d=class{constructor(){this.baseUrl=`http://localhost:3000`}getParkings(){return`${this.baseUrl}/parkings`}getParkingById(e){return`${this.baseUrl}/parkings/${e}`}getParkingsByTitle(e){return`${this.baseUrl}/parkings?title=${e}`}createParking(){return`${this.baseUrl}/parkings`}removeParkingById(e){return`${this.baseUrl}/parkings/${e}`}updateParkingById(e){return`${this.baseUrl}/parkings/${e}`}},f=new d})),m,h=e((()=>{a(),C(),s(),u(),p(),m=class{constructor(e,t){this.parent=e,this.cardID=t}get pageRoot(){return document.getElementById(`parking-page`)}getHTML(){return`
+                <div id="parking-page"></div>
+            `}getData(){l.get(f.getParkingById(this.cardID),e=>{this.renderData(e)})}renderData(e){new i(this.pageRoot).render(e)}render(){this.parent.innerHTML=``;let e=this.getHTML();this.parent.insertAdjacentHTML(`beforeend`,e),new o(this.pageRoot).render(),this.getData()}}})),g,_=e((()=>{g=class{constructor(e){this.parent=e}getHTML(){return`
+        <div style="display: grid; justify-content: center">
+            <div class="card_control">
+                <h5 style="text-align: center">Управление карточками</h5>
+                <table style="margin-left: 10px">
+                    <tr>
+                        <td style="width: 210px; padding-top: 10px">
+                            <p>Добавить новую карточку:</p>
+                        </td>
+                        <td>
+                            <button class="btn btn-primary" id="new_card_accept">Добавить</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 210px; padding-top: 10px">
+                            <p>Найти карточку:</p>
+                        </td>
+                        <td style="width: 210px">
+                            <input class="inp-primary" type="text" id="find_card_title">
+                        </td>
+                        <td>
+                            <button class="btn btn-primary" id="filter_accept">Найти</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        `}addAddListener(e){document.getElementById(`new_card_accept`).addEventListener(`click`,e)}addFilterListener(e){document.getElementById(`filter_accept`).addEventListener(`click`,e)}render(e,t){let n=this.getHTML();this.parent.insertAdjacentHTML(`beforeend`,n),this.addAddListener(e),this.addFilterListener(t)}}})),v,y=e((()=>{v=class{constructor(e){this.parent=e}getCardHTML(){return`
+            <div class="card">
+                <img class="card-img-top" src="" alt="Preview of new image">
+                <div class="card-body">
+                    <input class="inp-primary" type="text" id="new_card_title" style="width: 270px; margin-bottom: 7px">
+                    <input class="inp-primary" type="text" id="new_card_desc" style="width: 270px; margin-bottom: 7px">
+                    <table>
+                        <tr>
+                            <td>
+                                <button class="btn btn-primary" id="" data-id="">Припарковать</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-primary" id="" data-id="">Удалить</button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        `}render(e){let t=this.getCardHTML();this.parent.insertAdjacentHTML(`beforeend`,t),document.getElementById(`new_card_title`).value=e.title,document.getElementById(`new_card_desc`).value=e.text,document.getElementsByClassName(`card-img-top`)[0].src=e.src}}})),b,x=e((()=>{y(),C(),s(),u(),p(),b=class{constructor(e,t,n){this.parent=e,this.cardID=t,this.isEdit=n,this.jdata=``,this.imageChanged=!1}get pageRoot(){return document.getElementById(`card-edit-page`)}getHTML(){return`
+                <div id="card-edit-page">
+                    <div style="display: flex; justify-content: center; flex-direction: column; align-items: center", id="edit-space">
+                        <h2 style="margin-bottom: 20px">${this.isEdit?`Реадктирование`:`Добавление`} карточки</h2>
+                    </div>
+
+                    <div style="display: flex; justify-content: center; flex-direction: column; align-items: center">
+                        <table style="margin-top: 40px">
+                            <tr>
+                                <td style="padding-top: 7px; padding-right: 20px">
+                                    <p>Добавление картинки:</p>
+                                </td>
+                                <td style="padding-top: 7px">
+                                    <input class="inp-primary" type="text" id="new_image_src" style="width: 270px; margin-bottom: 7px">
+                                </td>
+                            </tr>
+                        </table>
+                    <button class="btn btn-primary" id="complete">Завершить редактирование</button>
+                </div>
+            `}getData(){this.isEdit?l.get(f.getParkingById(this.cardID),e=>{this.renderData(e),this.jdata=JSON.stringify(e)}):this.renderData({title:``,text:``,src:``})}changeImage(){document.getElementsByClassName(`card-img-top`)[0].src=document.getElementById(`new_image_src`).value,this.imageChanged=!0}completeEditing(){let e={src:document.getElementsByClassName(`card-img-top`)[0].src,title:document.getElementById(`new_card_title`).value,text:document.getElementById(`new_card_desc`).value,im_desc:`Своё изображение`,page_src:``};if(this.isEdit){let t=JSON.parse(this.jdata);console.log(this.imageChanged),this.imageChanged||(e.src=t.src),e.im_desc=t.im_desc,e.page_src=t.page_src,l.patch(f.updateParkingById(this.cardID),e,e=>{console.log(e)})}else l.post(f.createParking(),e,e=>{console.log(e)});new S(document.getElementById(`root`)).render()}addIChangeListener(){document.getElementById(`select-image`).addEventListener(`click`,this.changeImage.bind(this))}addCompleteListener(){document.getElementById(`complete`).addEventListener(`click`,this.completeEditing.bind(this))}renderData(e){new v(document.getElementById(`edit-space`)).render(e)}render(){this.parent.innerHTML=``;let e=this.getHTML();this.parent.insertAdjacentHTML(`beforeend`,e),new o(this.pageRoot).render(),this.getData(),this.addIChangeListener(),this.addCompleteListener()}}})),S,C=e((()=>{r(),h(),_(),s(),x(),u(),p(),S=class{constructor(e){this.parent=e}pageRoot(){return document.getElementById(`main-page`)}getHTML(){return`
+                <div id="main-page" class="d-flex flex-wrap" style="justify-content: center"><div/>
+            `}getCardsData(){l.get(f.getParkings(),e=>{this.renderCards(e)})}clickCard(e){let t=e.target.dataset.id;new m(this.parent,t).render()}editCard(e){let t=e.target.dataset.id;new b(this.parent,t,!0).render()}deleteCard(e){let t=e.target.dataset.id;l.delete(f.removeParkingById(t),e=>{console.log(e),this.render()})}addCard(e){new b(this.parent,0,!1).render()}findCard(e){this.pageRoot().innerHTML=``,l.get(f.getParkingsByTitle(document.getElementById(`find_card_title`).value),e=>{this.renderCards(e)})}renderCards(e){e.forEach(e=>{new n(this.pageRoot()).render(e,this.clickCard.bind(this),this.deleteCard.bind(this),this.editCard.bind(this))})}render(){this.parent.innerHTML=``,new o(this.parent).render(),new g(this.parent).render(this.addCard.bind(this),this.findCard.bind(this));let e=this.getHTML();this.parent.insertAdjacentHTML(`beforeend`,e),this.getCardsData()}}}));t((()=>{C(),new S(document.getElementById(`root`)).render(!0)}))();
